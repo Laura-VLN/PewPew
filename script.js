daebe7180d6e508d86b060fe0d01a686364075e6
Laura-VLN <lauravilain.pro@gmail.com>
2021-02-28 17:32:47 +0100

    pacman adaptation + ghost + projectile

--- script.js
+++ script.js
@@ -6,43 +6,85 @@ canvas.height = 750;
 
 var x = 480;
 var y = 710;
-var dx = 2;
+var dx = 5;
 
-function drawCannon() {
+function drawPacman() {
+    ctx.beginPath();
+    ctx.arc(x, y, 20, (Math.PI/180)*290, (Math.PI/180)*250, false);
+    ctx.lineTo(x, y);
+    ctx.fill();
+    ctx.closePath();
+}
 
-    ctx.fillRect(x, y, 40, 40);
+function drawGhost() {
 
+    ctx.beginPath();
+    ctx.moveTo(83, 116);
+    ctx.lineTo(83, 102);
+    ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
+    ctx.bezierCurveTo(105, 88, 111, 94, 111, 102);
+    ctx.lineTo(111, 116);
+    ctx.lineTo(106.333, 111.333);
+    ctx.lineTo(101.666, 116);
+    ctx.lineTo(97, 111.333);
+    ctx.lineTo(92.333, 116);
+    ctx.lineTo(87.666, 111.333);
+    ctx.lineTo(83, 116);
+    ctx.fill();
+
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.moveTo(91, 96);
+    ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
+    ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
+    ctx.bezierCurveTo(94, 106, 95, 103, 95, 101);
+    ctx.bezierCurveTo(95, 99, 94, 96, 91, 96);
+    ctx.moveTo(103, 96);
+    ctx.bezierCurveTo(100, 96, 99, 99, 99, 101);
+    ctx.bezierCurveTo(99, 103, 100, 106, 103, 106);
+    ctx.bezierCurveTo(106, 106, 107, 103, 107, 101);
+    ctx.bezierCurveTo(107, 99, 106, 96, 103, 96);
+    ctx.fill();
+
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.arc(101, 102, 2, 0, Math.PI * 2, true);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(89, 102, 2, 0, Math.PI * 2, true);
+    ctx.fill();
+    
 }
 
 function drawProjectile() {
-
     ctx.beginPath();
-    ctx.arc(50, 50, 10, 0, Math.PI*2);
+    ctx.arc(50, 50, 4, 0, Math.PI*2);
     ctx.fill();
     ctx.closePath();
-
 }
 
 function draw() {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawCannon();
+    drawPacman();
 
-    if ((x + 40) < canvas.width) {
+    if ((x + 20) < (canvas.width - 10)) {
         x += dx;
     }
-    if ((x + 40) == canvas.width) {
-        dx = -2;
+    if ((x + 20) == (canvas.width - 10)) {
+        dx = -5;
         x += dx;
     }
-    if (x == 0) {
-        dx = 2;
+    if ((x - 20) == 10) {
+        dx = 5;
         x += dx;
     }
 
     drawProjectile();
+    drawGhost();
 
-    window.requestAnimationFrame(draw);
+    window.requestAnimationFrame(draw); //plus fluide que setInterval -> s'adapte aux capacités du navigateur
 
 }
 
