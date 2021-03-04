0139ba4a75c7529ec5a366e892410ae736d1a5cc
Laura-VLN <lauravilain.pro@gmail.com>
2021-03-04 17:35:26 +0100

    ghost repop - first update

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ canvas.bottom = canvas.top + canvas.height;
 
 let projectiles = [];
 let projectiles_count = 0;
+let ghosts = [];
+let ghosts_count = 0;
 
 let randomX = (Math.floor(Math.random() * 960) + 40);
 let randomY = (Math.floor(Math.random() * 335) + 40);
@@ -46,57 +48,26 @@ class Projectile {
     }
 }
 
-function projectileDestroy() {
-    if (projectile.posY < 0) {projectiles.shift()};
-}
-
 class Ghost {
-    constructor(x, y) {
+    constructor(id, x, y) {
+        this.id = id;
         this.x = x;
         this.y = y;
     }
     draw() {
-        /*ctx.beginPath();
-        ctx.moveTo(83, 116);
-        ctx.lineTo(83, 102);
-        ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
-        ctx.bezierCurveTo(105, 88, 111, 94, 111, 102);
-        ctx.lineTo(111, 116);
-        ctx.lineTo(106.333, 111.333);
-        ctx.lineTo(101.666, 116);
-        ctx.lineTo(97, 111.333);
-        ctx.lineTo(92.333, 116);
-        ctx.lineTo(87.666, 111.333);
-        ctx.lineTo(83, 116);
-        ctx.fill();*/
-
         ctx.beginPath();
         ctx.moveTo(this.x, this.y);
         ctx.lineTo(this.x, this.y-14);
         ctx.bezierCurveTo(this.x, this.y-22, this.x+6, this.y-28, this.x+14, this.y-28);
         ctx.bezierCurveTo(this.x+18, this.y-28, this.x+28, this.y-22, this.x+28, this.y-14);
-        ctx.lineTo(this.x+28, this.y);
+        ctx.lineTo(this.x+28, this.y); //
         ctx.lineTo(this.x+23.333, this.y-4.667);
-        ctx.lineTo(this.x+18.666, this.y);
+        ctx.lineTo(this.x+18.666, this.y); //
         ctx.lineTo(this.x+14, this.y-4.667);
-        ctx.lineTo(this.x+9.333, this.y);
+        ctx.lineTo(this.x+9.333, this.y); //
         ctx.lineTo(this.x+4.666, this.y-4.667);
-        ctx.lineTo(this.x, this.y);
+        ctx.lineTo(this.x, this.y); //
         ctx.fill();
-
-        /*ctx.fillStyle = "white";
-        ctx.beginPath();
-        ctx.moveTo(91, 96);
-        ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
-        ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
-        ctx.bezierCurveTo(94, 106, 95, 103, 95, 101);
-        ctx.bezierCurveTo(95, 99, 94, 96, 91, 96);
-        ctx.moveTo(103, 96);
-        ctx.bezierCurveTo(100, 96, 99, 99, 99, 101);
-        ctx.bezierCurveTo(99, 103, 100, 106, 103, 106);
-        ctx.bezierCurveTo(106, 106, 107, 103, 107, 101);
-        ctx.bezierCurveTo(107, 99, 106, 96, 103, 96);
-        ctx.fill();*/
 
         ctx.fillStyle = "white";
         ctx.beginPath();
@@ -112,15 +83,6 @@ class Ghost {
         ctx.bezierCurveTo(this.x+24, this.y-17, this.x+23, this.y-20, this.x+20, this.y-20);
         ctx.fill();
 
-        /*ctx.fillStyle = "black";
-        ctx.beginPath();
-        ctx.arc(101, 102, 2, 0, Math.PI * 2, true);
-        ctx.fill();
-
-        ctx.beginPath();
-        ctx.arc(89, 102, 2, 0, Math.PI * 2, true);
-        ctx.fill();*/
-
         ctx.fillStyle = "black";
         ctx.beginPath();
         ctx.arc(this.x+18, this.y-14, 2, 0, Math.PI * 2, true);
@@ -132,25 +94,49 @@ class Ghost {
     }
 }
 
-function createGhost() {
-    ghost = new Ghost(randomX, randomY);
-    ghost.draw();
+function createProjectile() {
+    projectile = new Projectile(projectiles_count, pacman.x, pacman.y, 3);
+    projectiles.push(projectile);
+    projectile.draw();
+    projectiles_count += 1;
 }
+
+function projectileDestroy() {
+    if (projectile.posY < 0) {projectiles.shift()};
+}
+
+function createGhost() {
+    ghost = new Ghost(ghosts_count, randomX, randomY);
+    ghosts.push(ghost);
+    ghost.draw();
+    ghosts_count += 1;
+}
+
+function ghostRepop() {
+    if (projectile.posX >= ghost.x && projectile.posX <= ghost.x+28 && projectile.posY >= ghost.y) {
+        ghosts.shift()
+        createGhost();
+    }
+}
+console.log(ghosts)
+console.log(randomX)
+console.log(randomY)
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     pacman.draw();
+    ghost.draw();
     
     projectiles.forEach(projectile => {
         projectile.draw();
         projectile.move();
         projectileDestroy();
+        ghostRepop();
     });
 
-    createGhost();
-
     window.requestAnimationFrame(draw); //plus fluide que setInterval -> s'adapte aux capacités du navigateur
+
 }
 
 canvas.addEventListener('mousemove', function(e) {
@@ -164,10 +150,7 @@ canvas.addEventListener('mousemove', function(e) {
 });
 
 canvas.addEventListener('click', () => {
-    projectile = new Projectile(projectiles_count, pacman.x, pacman.y, 3);
-    projectiles.push(projectile);
-    projectile.draw();
-    projectiles_count += 1;
+    createProjectile();
 });
 
 window.requestAnimationFrame(draw);
